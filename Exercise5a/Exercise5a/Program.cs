6928de413904063bff5be85a69bf235e63e2e51d
PhuHung <hung.187pm06602@vanlanguni.vn>
2019-10-08 17:25:26 +0700

    finish problem 1

--- Exercise5a/Exercise5a/Program.cs
+++ Exercise5a/Exercise5a/Program.cs
@@ -10,6 +10,41 @@ namespace Exercise5a
     {
         static void Main(string[] args)
         {
+            //Print_a_to_z_v1();
+            Print_a_to_z_v2();
+           // Print_z_to_a_v1();
+        }
+        public static void Print_a_to_z_v1()
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+            Console.WriteLine();
+        }
+        public static void Print_a_to_z_v2()
+        {
+            for (char c = 'a'; c <= 'z'; c++ )
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine();
+        }
+        public static void Print_z_to_a_v1()
+        {
+            for (int i = 122; i > 97; i--)
+            {
+                Console.WriteLine((char)i);
+            }
+            Console.WriteLine();
+        }
+        public static void Print_A_to_Z_v1()
+        {
+            for(int i = 65 ; i<=90 ; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+            Console.WriteLine();
         }
     }
 }
